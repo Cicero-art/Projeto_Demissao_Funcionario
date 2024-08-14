ece20a0f1fc68308d2292b5070dccfe583d3509d
Cicero-art <ciceroneto28@gmail.com>
2024-08-13 23:39:43 -0300

    Adiciona verificação de parâmetros e docstring à função analise_bivariada
    - Adiciona descrição dos parâmetros da função de rótulo
    - Transformação do import do repositório da máquina para o repositório do projeto

--- scripts/analise_vr.py
+++ scripts/analise_vr.py
@@ -1,32 +1,38 @@
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+from pathlib import Path
 
-# Ler o arquivo do diretório
-df = pd.read_csv("C:/Users/cicero.neto/Projeto_Demissao_Funcionario/data/HR_comma_sep.csv")
-print(type(df))
-# Contar, normalizar e adicioncar um dataframe para visualização da distribuição
+# Definir o caminho do arquivo de forma relativa ao repositório
+file_path = Path(__file__).parent.parent / "data" / "HR_comma_sep.csv"
+
+# Carregar somente a coluna 'left' do arquivo CSV
+df = pd.read_csv(file_path, usecols=['left'])
+
+# Contar a frequência de cada valor na coluna 'left' e normalizar (convertendo para percentuais)
 contagem = df['left'].value_counts(normalize=True) * 100
 dist_df = contagem.reset_index()
+
+# Renomear as colunas do DataFrame para nomes mais descritivos
 dist_df.columns = ['left', 'Porcentagem']
 
-# Visualização da distribuição
+# Criar um gráfico de barras para visualizar a distribuição da variável 'left'
 grafico = sns.barplot(data=dist_df, x='left', y='Porcentagem')
-plt.xlabel('valor')
+
+plt.xlabel('Valor')
 plt.ylabel('Percentual (%)')
 plt.title('Distribuição da Variável target "Left"')
 
+# Adicionar rótulos de porcentagem em cima de cada barra no gráfico
 for p in grafico.patches:
-    # Adicionar o rótulo de dados
     grafico.annotate(
-        f'{p.get_height():.1f}%',
-        (p.get_x() + p.get_width() / 2.,
-         p.get_height()),
-        ha='center',
-        va='center',
-        xytext=(0, -10),
-        textcoords='offset points',
-        fontsize=10
+        f'{p.get_height():.1f}%',  # Rótulo formatado para exibir uma casa decimal
+        (p.get_x() + p.get_width() / 2., p.get_height()),  # Posição do rótulo
+        ha='center',  # Alinhamento horizontal ao centro da barra
+        va='center',  # Alinhamento vertical ao centro da barra
+        xytext=(0, -10),  # Deslocamento do rótulo em relação ao ponto calculado
+        textcoords='offset points',  # Unidades de deslocamento dos rótulos
+        fontsize=10  # Tamanho da fonte do rótulo
     )
 
 plt.show()
